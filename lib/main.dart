086269c276424823c1075dbcbf8a435d17d194b1
Mukesh <mukeshkachhawah49@gmail.com>
2023-08-21 11:41:25 +0530

    change title

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Cubit TODO List',
       theme: ThemeData(
        
         primarySwatch: Colors.blue,
@@ -25,4 +25,5 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 
